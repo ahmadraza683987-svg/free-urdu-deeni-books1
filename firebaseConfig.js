f1c08a2147bf538a30f2c073813921169c043e54
ahmadraza683987-svg <ahmadraza683987@gmail.com>
2025-10-02 01:56:28 +0530

    Add firebaseConfig.js for Firebase setup

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+// firebaseConfig.js
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCDvXaKGUjYwc2Ly4PBBoKJVobdulVs8js",
+  authDomain: "free-urdu-deeni-books-comments.firebaseapp.com",
+  projectId: "free-urdu-deeni-books-comments",
+  storageBucket: "free-urdu-deeni-books-comments.appspot.com",
+  messagingSenderId: "364160557039",
+  appId: "1:364160557039:web:583ab4296a72a4eeb2a01a"
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
